54f05262797d2390b4cfb5c90639d72f24869739
Fayel MOHAMED <mohamed.fayel@yahoo.com>
2025-06-25 10:02:44 +0200

    Cms

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -4,15 +4,16 @@
       <p class="font-bold col-start-2 text-2xl m-2">Liste des Films</p>
       <button
         class="rounded-lg bg-blue-500 col-start-3 text-white p-2 hover:bg-blue-700"
-        @click="logout()">
+        @click="logout">
         Se déconnecter
       </button>
     </div>
     <div v-if="loading">Chargement...</div>
     <div v-else class="grid grid-cols-4 gap-4 m-5">
-      <button @click="$router.push(`film/${movie.documentId}`)" class="hover:bg-gray-100 ">
+      
         <div v-for="movie in movies" :key="movie.id"
           class="border-1 border-gray-300 rounded-lg flex flex-col items-center p-4 ">
+          <button @click="$router.push(`film/${movie.documentId}`)" class="hover:bg-gray-100 ">
           <img v-if="movie.image && movie.image.url"
             :src="movie.image.url.startsWith('http') ? movie.image.url : `${API_URL.replace('/api', '')}${movie.image.url}`"
             :alt="movie.title" style="max-width: 200px; display: block; margin-bottom: 1rem;" />
@@ -26,9 +27,8 @@
             </span>
           </p>
           <p><strong>Année: </strong> {{ movie.year }}</p>
-
+          </button>
         </div>
-      </button>
     </div>
   </div>
 </template>
@@ -36,6 +36,7 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 import axios from 'axios'
+import router from '@/router'
 
 const movies = ref([])
 const loading = ref(true)
@@ -46,8 +47,8 @@ const API_URL = 'http://localhost:1337/api'
 // Remplacez ceci par la gestion réelle du token après connexion
 const token = localStorage.getItem('jwt')
 
-function logout(){
-  $router.push('/login')
+const logout= ()=>{
+  router.push({ name: 'login' })
   localStorage.removeItem('jwt')
 }
 
